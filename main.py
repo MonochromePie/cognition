b774be7773e6f46fb24cc41a9c1a073550097787
natapol2547 <robloxplay41@gmail.com>
2026-02-04 23:11:21 +0700

    oklab color space

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from utils.image import read_image, write_image, resize_image, convolution, create_gaussian_kernel
-from utils.color_space import rgb_to_oklch, oklch_to_rgb
+from utils.color_space import rgb_to_oklab, oklab_to_rgb
 import time
 
 def main():
     image = read_image("frame1.jpg")
     blurred_image = resize_image(image, 100, 75)
-    oklch_image = rgb_to_oklch(blurred_image)
-    lightness = oklch_image[..., 1]
-    write_image("oklch_image.jpg", lightness*255)
-    rgb_image = oklch_to_rgb(oklch_image)
+    oklab_image = rgb_to_oklab(blurred_image)
+    lightness = oklab_image[..., 0]
+    write_image("oklab_image.jpg", lightness*255)
+    rgb_image = oklab_to_rgb(oklab_image)
     write_image("rgb_image.jpg", rgb_image)
     # gaussian_kernel = create_gaussian_kernel(3, 1)
     # for _ in range(10):
